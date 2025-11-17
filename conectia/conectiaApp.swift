8d542da587d470268d106b01f9c063600fea3049
emiliano zapata <emilianozapatarestrepo@gmail.com>
2025-11-17 14:00:34 -0500

    Initial Commit

---
+++ conectia/conectiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  conectiaApp.swift
+//  conectia
+//
+//  Created by Emiliano zapata on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct conectiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
